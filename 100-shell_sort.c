ff819b3ec9198418f72dc4e198d36e4a1598f749
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-10-28 22:34:15 +0100

    add

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -20,22 +20,23 @@ void swap(int *one, int *two)
  */
 void shell_sort(int *array, size_t size)
 {
+	size_t knuth_gap, outer, inner;
+	int elements_swapped = 0;
+    int current_element = array[outer];
+    size_t inner_iterator;
+
     if (array == NULL || size < 2)
         return;
-
-    size_t knuth_gap, outer, inner;
 
     for (knuth_gap = 1; knuth_gap < size / 3; knuth_gap = 3 * knuth_gap + 1);
 
     do
     {
-        int elements_swapped = 0;
 
         for (outer = knuth_gap; outer < size; outer++)
         {
-            int current_element = array[outer];
-            size_t inner_iterator;
 
+            current_element = array[outer];
             for (inner_iterator = outer; inner_iterator >= knuth_gap && array[inner_iterator - knuth_gap] > current_element; inner_iterator -= knuth_gap)
             {
                 swap(&array[inner_iterator], &array[inner_iterator - knuth_gap]);
